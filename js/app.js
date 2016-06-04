a23cb5f0627339e0ff1dd1638b5eaa111e78dc27
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-06-04 09:56:21 -0400

    finished project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-
+	for(var i=1; i < 101; i++) {
+		//console.log(i)
+	if(i % 3 == 0 && i % 5 ==0) {
+		$(".numbers").append("<p>fizzbuzz</p>");
+	}
+	else if(i % 3 == 0) {
+		$(".numbers").append("<p>fizz</p>");
+	}
+	else if (i % 5 == 0) {
+		$(".numbers").append("<p>buzz</p>");
+	}
+	else {
+		$(".numbers").append("<p>"+i+"</p>");
+	}
+}
 });
